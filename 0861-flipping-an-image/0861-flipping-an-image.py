e7e0c7ac6687bd5475d63d56b400b4c36ffc25ef
Aditya Kamath <75475382+adityakamath1997@users.noreply.github.com>
2025-05-20 18:35:01 -0400

    Time: 0 ms (100%), Space: 17.9 MB (38.33%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.py
+++ 0861-flipping-an-image/0861-flipping-an-image.py
@@ -4,8 +4,7 @@ class Solution:
         for i in range(n):
             for j in range(ceil(n/2)):
                 image[i][j],image[i][n-j-1]=image[i][n-j-1],image[i][j]
-        
-        for i in range(n):
             for j in range(n):
                 image[i][j]=1-image[i][j]
+            
         return image
