21d7aa96695df55941ccd28f071619c8bee41ce4
Aditya Kamath <75475382+adityakamath1997@users.noreply.github.com>
2025-05-20 00:50:19 -0400

    Time: 0 ms (100%), Space: 18.6 MB (94.07%) - LeetHub

--- 0792-binary-search/0792-binary-search.py
+++ 0792-binary-search/0792-binary-search.py
@@ -1,6 +1,7 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        L,R=0,len(nums)-1
+        L=0
+        R=len(nums)-1
 
         while L<=R:
             mid=(L+R)//2
@@ -8,7 +9,6 @@ class Solution:
                 L=mid+1
             elif target<nums[mid]:
                 R=mid-1
-            else:
+            elif target==nums[mid]:
                 return mid
         return -1
-        
