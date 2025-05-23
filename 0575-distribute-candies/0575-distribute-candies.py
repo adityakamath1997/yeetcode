d9e61b4c102c888c6d54d9676aedfaca581a6fad
Aditya Kamath <75475382+adityakamath1997@users.noreply.github.com>
2025-05-23 05:02:47 -0400

    Time: 18 ms (71.51%), Space: 14.3 MB (12.32%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.py
+++ 0575-distribute-candies/0575-distribute-candies.py
@@ -1,11 +1,6 @@
 class Solution(object):
     def distributeCandies(self, candyType):
-        n=len(candyType)
-        max_candies=n/2
-        coll=set()
-        for i in candyType:
-            coll.add(i)
-
-        return min(max_candies,len(coll))
+        unique=set(candyType)
+        return min(len(unique),len(candyType)/2)
 
         
