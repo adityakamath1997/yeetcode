9150f0fa187a9c53c43e93bebce73e27278dd718
Aditya Kamath <75475382+adityakamath1997@users.noreply.github.com>
2025-05-23 23:15:41 -0400

    Time: 3 ms (59.71%), Space: 13.1 MB (77.28%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,10 @@
 class Solution(object):
     def twoSum(self, nums, target):
         m={}
-        for idx, val in enumerate(nums):
+
+        for idx,val in enumerate(nums):
             diff=target-val
             if diff in m:
                 return [m[diff],idx]
             m[val]=idx
-        return []
+        
