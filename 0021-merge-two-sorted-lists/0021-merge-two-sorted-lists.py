d0cdef6502e2737ecfc0290f47c3dce30f895180
Aditya Kamath <75475382+adityakamath1997@users.noreply.github.com>
2025-06-05 10:58:15 -0400

    Time: 0 ms (100%), Space: 17.5 MB (98.53%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -1,35 +1,29 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def mergeTwoLists(self, list1, list2):
-        """
-        :type list1: Optional[ListNode]
-        :type list2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        dummy=ListNode()
-        head=dummy
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
 
-        while list1 and list2:
-            val1 = list1.val 
-            val2 = list2.val 
+        dummy = ListNode()
+        curr = dummy
+        curr1 = list1
+        curr2 = list2
 
-            if val1>=val2:
-                head.next=list2
-                list2=list2.next
+        while curr1 and curr2:
+            val1 = curr1.val
+            val2 = curr2.val
+
+            if val1 >= val2:
+                curr.next = curr2
+                curr2 = curr2.next
             else:
-                head.next=list1
-                list1=list1.next
-            head=head.next
+                curr.next = curr1
+                curr1 = curr1.next
+            curr = curr.next
         
-        head.next= list1 or list2
-            
-            
+        curr.next = curr1 or curr2
+
         return dummy.next
-
-
-
         
