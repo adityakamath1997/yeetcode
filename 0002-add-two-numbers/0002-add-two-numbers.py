17e45c079b7e31d01b94a3002b0ec3c4179d88c3
Aditya Kamath <75475382+adityakamath1997@users.noreply.github.com>
2025-06-06 02:28:41 -0400

    Time: 7 ms (32.52%), Space: 17.9 MB (35.92%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -1,45 +1,35 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        dummy = ListNode()
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
         curr = dummy
-        carry = 0
+
         curr1 = l1
         curr2 = l2
+        carry = 0
 
         while curr1 or curr2:
-            val1 = curr1.val if curr1 else 0  # In case one of them has ended and val is 0
+            val1 = curr1.val if curr1 else 0
             val2 = curr2.val if curr2 else 0
 
-            # Calculating quotient and carry:
+            node = (val1 + val2 + carry) % 10
+            carry = (val1 + val2 + carry) // 10
 
-            quotient = (carry + val1 + val2) % 10
-            carry = (carry + val1 + val2) // 10
-
-            curr.next = ListNode(quotient)
+            curr.next = ListNode(node)
+            curr = curr.next
 
             if curr1:
                 curr1 = curr1.next
             if curr2:
                 curr2 = curr2.next
-            
-            curr = curr.next
 
         if carry != 0:
             curr.next = ListNode(carry)
-            curr = curr.next
-        
+
         return dummy.next
-
-
-
-
-
-
-
 
         
