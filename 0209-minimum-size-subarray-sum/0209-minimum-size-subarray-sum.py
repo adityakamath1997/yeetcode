56989a277e394ab27bec6c05d50c03df316cc25b
Aditya Kamath <75475382+adityakamath1997@users.noreply.github.com>
2025-06-06 03:06:28 -0400

    Time: 8 ms (67.23%), Space: 28.4 MB (37.46%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -3,16 +3,12 @@ class Solution:
 
         running_sum = 0
         L = 0
-        min_length = 10 ** 6
+        length = 10 ** 6
         for R in range(len(nums)):
             running_sum += nums[R]
             while running_sum >= target:
-                W = (R - L) + 1
-                min_length = min(min_length, W)
+                W = R - L + 1
+                length = min(length, W)
                 running_sum -= nums[L]
                 L += 1
-        
-        if min_length == 10 ** 6:
-            return 0
-        else:
-            return min_length
+        return 0 if length == 10 ** 6 else length
