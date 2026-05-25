6fc8d46268f6b20409fa64e9ebf4746af3a7b7d2
Aditya <75475382+adityakamath1997@users.noreply.github.com>
2026-05-25 02:54:49 -0400

    0383-ransom-note: 05-25-2026

---
+++ LeetCode/Easy/0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        counter = dict()
+
+        for char in magazine:
+            counter[char] = counter.get(char, 0) + 1
+
+        for char in ransomNote:
+            if char not in counter or counter[char] <= 0:
+                return False
+            counter[char] -= 1
+        
+        return True
