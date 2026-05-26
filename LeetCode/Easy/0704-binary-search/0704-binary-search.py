1075dd47bda6c442b01c84dccb0f5f96e7598370
Aditya <75475382+adityakamath1997@users.noreply.github.com>
2026-05-26 04:51:36 -0400

    0704-binary-search: 05-26-2026

--- LeetCode/Easy/0704-binary-search/0704-binary-search.py
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.py
@@ -4,11 +4,13 @@ class Solution:
 
         while l <= r:
             mid = (l + r) // 2
+
             if nums[mid] > target:
                 r = mid - 1
             elif nums[mid] < target:
                 l = mid + 1
             else:
                 return mid
-
+        
         return -1
+        
