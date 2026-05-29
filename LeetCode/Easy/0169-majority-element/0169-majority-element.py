ffae18225885c3a7373cec046c231dd1b52f2784
Aditya <75475382+adityakamath1997@users.noreply.github.com>
2026-05-28 21:31:31 -0400

    0169-majority-element: 05-28-2026

--- LeetCode/Easy/0169-majority-element/0169-majority-element.py
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.py
@@ -1,8 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+
         n = len(nums)
         temp = dict()
-        count = n / 2
+        count = n // 2
 
         for num in nums:
             temp[num] = temp.get(num, 0) + 1
