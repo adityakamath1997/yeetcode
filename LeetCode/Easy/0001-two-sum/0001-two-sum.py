987de6be7182a4605a93d35a12c49631ea6fa47e
Aditya <75475382+adityakamath1997@users.noreply.github.com>
2026-05-29 20:39:48 -0400

    0001-two-sum: 05-29-2026

--- LeetCode/Easy/0001-two-sum/0001-two-sum.py
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.py
@@ -1,12 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        temp = dict()
+        seen = dict()
 
         for i, num in enumerate(nums):
             diff = target - num
-            if diff in temp:
-                return [i, temp[diff]]
-            temp[num] = i
+            if diff in seen:
+                return [i, seen[diff]]
+            seen[num] = i
 
+        return []
         
