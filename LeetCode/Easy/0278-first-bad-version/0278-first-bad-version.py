5b1059360a6686638a5ccb6ecdf14d7d09b7e4be
Aditya <75475382+adityakamath1997@users.noreply.github.com>
2026-05-29 20:38:14 -0400

    0278-first-bad-version: 05-29-2026

--- LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.py
+++ LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.py
@@ -6,11 +6,11 @@ class Solution:
         l, r = 1, n
 
         while l < r:
-            mid = (l + r) // 2
+            mid = l + (r - l) // 2
+
             if isBadVersion(mid):
                 r = mid
             else:
                 l = mid + 1
-        
+
         return l
-        
