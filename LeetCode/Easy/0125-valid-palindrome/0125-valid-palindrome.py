2b1a3360d31532935f1f3d03f26d09e88f89dc2c
Aditya <75475382+adityakamath1997@users.noreply.github.com>
2026-07-14 23:46:45 -0400

    0125-valid-palindrome: 07-14-2026

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,13 +1,18 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = ''.join(char.lower() for char in s if char.isalnum())
-
         l, r = 0, len(s) - 1
 
         while l < r:
-            if s[l] != s[r]:
+            while l < r and not s[l].isalnum():
+                l += 1
+            
+            while l < r and not s[r].isalnum():
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
                 return False
+            
             l += 1
             r -= 1
-        
+
         return True
