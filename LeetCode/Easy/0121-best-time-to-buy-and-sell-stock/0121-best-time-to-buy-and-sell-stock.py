3ba08c490e8d9f7d3868d15d2566fdaa1473b493
Aditya <75475382+adityakamath1997@users.noreply.github.com>
2026-07-24 00:43:00 -0400

    0121-best-time-to-buy-and-sell-stock: 07-24-2026

--- LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ LeetCode/Easy/0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) < 2:
-            return 0
-
-        l, r = 0, 1
+        left, right = 0, 1
         max_profit = 0
-        while r < len(prices):
-            profit = prices[r] - prices[l]
-            max_profit = max(max_profit, profit)
-            if profit <= 0:
-                l = r
-            r += 1
-        
-        return max_profit
 
+        while right < len(prices):
+            profit = prices[right] - prices[left]
+            max_profit = max(profit, max_profit)
+
+            if prices[right] <= prices[left]:
+                left = right
+
+            right += 1
+
+        return max_profit
         
